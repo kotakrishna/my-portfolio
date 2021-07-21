dad341aaa540f03adbd456cae39bc8977347a488
k.saikrishna <k.saikrishna78@gmail.com>
2021-07-21 21:16:32 +0530

    th esecond change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       {/* <NavBar /> */}
-      <VideoBackground/>
+      {/* <VideoBackground/> */}
       <Switch>
         <Route path="/">
           <Home />
